73cecdc22428e78c479f008becbeeecba7d25e00
Rafa90k <embau12rafael@gmail.com>
2025-05-20 20:38:41 -0300

    faet(GameList): Adicionando busca por id

--- src/main/java/com/gamesforyou/dslist/controllers/GameController.java
+++ src/main/java/com/gamesforyou/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gamesforyou.dslist.Dto.GameDTO;
 import com.gamesforyou.dslist.Dto.GameMinDTO;
 import com.gamesforyou.dslist.entities.Game;
 import com.gamesforyou.dslist.services.GameService;
@@ -17,6 +19,12 @@ public class GameController {
 	
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable long id){
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
 	
 	@GetMapping
 	public List<GameMinDTO> findAll(){
