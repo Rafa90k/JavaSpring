bdaac5384a71fe76cf00c92f04cea358f1481ef5
Rafa90k <embau12rafael@gmail.com>
2025-05-20 21:57:24 -0300

    feat(GameList): Criando o endpoint para trazer por tipo

--- src/main/java/com/gamesforyou/dslist/services/GameService.java
+++ src/main/java/com/gamesforyou/dslist/services/GameService.java
@@ -1,4 +1,5 @@
 package com.gamesforyou.dslist.services;
+import com.gamesforyou.dslist.projections.GameMinProjection;
 
 
 import java.util.List;
@@ -27,6 +28,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return  result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return  result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 	
