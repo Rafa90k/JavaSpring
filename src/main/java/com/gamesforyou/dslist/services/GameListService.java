88b899d1cccefb140aa0ecd3b1f4b51589a2dc82
Rafa90k <embau12rafael@gmail.com>
2025-05-22 20:39:42 -0300

    feat(GameLIst): Criando o metodo de ordenação de jogos

--- src/main/java/com/gamesforyou/dslist/services/GameListService.java
+++ src/main/java/com/gamesforyou/dslist/services/GameListService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.gamesforyou.dslist.Dto.GameListDTO;
 import com.gamesforyou.dslist.entities.GameList;
+import com.gamesforyou.dslist.projections.GameMinProjection;
 import com.gamesforyou.dslist.repositories.GameListRepository;
+import com.gamesforyou.dslist.repositories.GameRepository;
 
 
 @Service
@@ -14,10 +16,35 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){
 		List<GameList> result = gameListRepository.findAll();
 		return  result.stream().map(x -> new GameListDTO(x)).toList();
 	}
 	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public GameListDTO findById(Long id) {
+		GameList entity = gameListRepository.findById(id).get();
+		return new GameListDTO(entity);
+	}
 }
+	
